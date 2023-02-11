1b1437eb8bbedf56152317c628634c593de0b299
VolodymyrSalenko <vova.salenko@gmail.com>
2023-02-11 03:46:57 +0200

    Task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.body;
+let timerId = 0;
+
+
+startBtnEl.addEventListener('click', onChangeColorClick);
+stopBtnEl.addEventListener('click', onStopChangeColorClick);
+
+
+function onChangeColorClick() {
+    startBtnEl.setAttribute('disabled', 'true');
+    
+    timerId = setInterval(
+        () => {bodyEl.style.backgroundColor = getRandomHexColor()},
+        1000
+    );
+};
+
+function onStopChangeColorClick() {
+    clearInterval(timerId);
+    startBtnEl.removeAttribute('disabled');
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
